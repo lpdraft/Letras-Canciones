b99956c72846115e404f8af0fd6145764c9634f5
lpdraft <lpdraft001@gmail.com>
2021-10-18 19:28:02 +0200

    Subiendo de nuevo

---
+++ js/app.js
@@ -0,0 +1,29 @@
+import * as UI from './interfaz.js';
+import API from './api.js';
+
+UI.formularioBuscar.addEventListener('submit', buscarCancion);
+
+function buscarCancion(e){
+    e.preventDefault();
+
+    //Obtener datos del formulario
+    const artista = document.querySelector('#artista').value;
+    const cancion = document.querySelector('#cancion').value;
+
+    if( artista === ''|| cancion === '' ) {
+        //Mostrar el error al no cubrir todos los campos
+
+        UI.divMensajes.textContent = 'Error.. los dos campos son obligatorios';
+        UI.divMensajes.classList.add('error');
+
+        setTimeout(() => {
+            UI.divMensajes.textContent = '';
+            UI.divMensajes.classList.remove('error');
+        }, 3000);
+
+        return;
+    }
+    //Consultar nueva API
+    const busqueda = new API(artista, cancion);
+    busqueda.consultarAPI();
+}
